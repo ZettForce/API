67b5983af686e08bc5054ef956886c1daf6b0dd5
ZettForce <83797376+ZettForce@users.noreply.github.com>
2022-04-03 16:07:57 -0700

    Last

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb+srv://Admin123:pa$$word@cluster0.1x3te.mongodb.net/test';
+const URL = 'mongodb+srv://ZettForce:Zett1995@cluster0.nwau9.mongodb.net/api?retryWrites=true&w=majority';
 
 
 mongoose.connect(URL, {
--- routes/car_routes.js
+++ routes/car_routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const car_Controller = require('../controllers/car.controller');
+const car_Controller = require('../controllers/car_controller');
 
 router.get('/', car_Controller.getCars);
 router.post('/', car_Controller.addCar);
